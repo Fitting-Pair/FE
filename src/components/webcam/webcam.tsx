afed97ec6929334686ad4acce35d55f3a27b6255
hyewon <89793335+hijjoy@users.noreply.github.com>
2024-09-03 15:10:57 +0900

    ✨ feat: 카메라 MediaPipe (#22)
    - MediaPipe 관련 설치
    - 주먹 감지 후 사진촬영 로직 추가

--- src/components/webcam/webcam.tsx
+++ src/components/webcam/webcam.tsx
@@ -1,9 +1,9 @@
 import * as S from "./webcam.style";
-
 import CamImg from "@/assets/images/camera_img.webp";
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useRef, useState, useEffect } from "react";
 import { FaArrowRight } from "react-icons/fa6";
 import Webcam from "react-webcam";
+import { GestureRecognizer, FilesetResolver } from "@mediapipe/tasks-vision";
 import usePostImg from "@/hooks/queries/results/usePostImg";
 
 const videoConstraints = {
@@ -13,13 +13,79 @@ const videoConstraints = {
 
 const WebcamComponent = () => {
   const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false);
-  const webcamRef = useRef<Webcam>(null);
   const [url, setUrl] = useState<string | null>(null);
   const [formData, setFormData] = useState<FormData | null>(null);
-  const timerIdRef = useRef<number | null>(null);
   const [count, setCount] = useState<number>(5);
 
+  const webcamRef = useRef<Webcam>(null);
+  const timerIdRef = useRef<number | null>(null);
+
   const { mutate } = usePostImg();
+
+  useEffect(() => {
+    const loadGestureRecognizer = async () => {
+      const vision = await FilesetResolver.forVisionTasks(
+        "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm",
+      );
+      const gestureRecognizer = await GestureRecognizer.createFromOptions(
+        vision,
+        {
+          baseOptions: {
+            modelAssetPath:
+              "https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task",
+            delegate: "CPU",
+          },
+          runningMode: "VIDEO",
+        },
+      );
+
+      const predict = async () => {
+        if (webcamRef.current && webcamRef.current.video) {
+          const video = webcamRef.current.video;
+
+          if (video.videoWidth > 0 && video.videoHeight > 0) {
+            const results = await gestureRecognizer.recognizeForVideo(
+              video,
+              Date.now(),
+            );
+
+            if (results.gestures && results.gestures.length > 0) {
+              const gesture = results.gestures[0][0];
+              console.log(gesture);
+
+              if (gesture.categoryName === "Closed_Fist") {
+                console.log("주먹을 쥠");
+                startTimer();
+                return;
+              }
+            }
+          }
+        }
+        requestAnimationFrame(predict);
+      };
+
+      predict();
+    };
+
+    loadGestureRecognizer();
+  }, []);
+
+  const startTimer = () => {
+    if (timerIdRef.current) {
+      clearInterval(timerIdRef.current);
+    }
+    setCount(5);
+    timerIdRef.current = window.setInterval(() => {
+      setCount((count) => {
+        if (count === 0) {
+          clearInterval(timerIdRef.current as number);
+          capture();
+          return 5;
+        }
+        return count - 1;
+      });
+    }, 1000);
+  };
 
   const capture = useCallback(async () => {
     const imageSrc = webcamRef.current?.getScreenshot();
@@ -38,23 +104,6 @@ const WebcamComponent = () => {
     setCaptureEnable(false);
     setUrl(null);
     startTimer();
-  };
-
-  const startTimer = () => {
-    if (timerIdRef.current) {
-      clearInterval(timerIdRef.current);
-    }
-    setCount(5);
-    timerIdRef.current = setInterval(() => {
-      setCount((count) => {
-        if (count === 0) {
-          clearInterval(timerIdRef.current as number);
-          capture();
-          return 5;
-        }
-        return count - 1;
-      });
-    }, 1000);
   };
 
   const handleNextClick = () => {
