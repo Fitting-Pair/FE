389e23cd24527d943047c8d5ab6e67004c671f7d
hijjoy <hijjoy@naver.com>
2024-09-10 16:01:44 +0900

    ✨ feat: 타이머 크게 수정

--- src/pages/body-check/body-check.tsx
+++ src/pages/body-check/body-check.tsx
@@ -1,7 +1,6 @@
 import * as S from "./body-check.style";
 
 import { Icon, WebcamComponent } from "@/components";
-// import Webcam from "../../components/Webcam/Webcam";
 
 const BodyCheckPage = () => {
   return (
@@ -9,16 +8,6 @@ const BodyCheckPage = () => {
       <Icon blackNum={1} />
       <S.ContentContainer>
         <WebcamComponent />
-        <S.TipWrapper>
-          <S.TipIcon>{`Tip ) 아래 순서를 따라해주세요.`}</S.TipIcon>
-          <S.ContentWrapper>
-            <span>촬영 버튼을 누르고 5초 후에 촬영됩니다.</span>
-            <li>1. 정면을 바라보고 사진을 찍어주세요.</li>
-            <li>2. 옆구리가 붙지 않도록 팔을 벌려주세요.</li>
-            <li>3. 어깨넓이 만큼 다리를 벌려주세요.</li>
-            <li>4. 충분히 거리를 두고 발끝까지 나오게 찍어주세요.</li>
-          </S.ContentWrapper>
-        </S.TipWrapper>
       </S.ContentContainer>
     </S.Container>
   );
